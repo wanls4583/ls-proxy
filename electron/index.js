7fec2d55ddb71b8c9f72eb3bb04fe6a21d86f047
lisong <409051987@qq.com>
2024-07-17 11:32:40 +0800

    test spawn

--- electron/index.js
+++ electron/index.js
@@ -83,3 +83,21 @@ function initProtocol() {
     callback(decodeURI(path.normalize(url)));
   });
 }
+
+// const { spawn } = require('node:child_process');
+// const ls = spawn('./server', [], {
+//   shell: true,
+//   cwd: path.join(app.getAppPath(), 'electron/proxy')
+// });
+
+// ls.stdout.on('data', (data) => {
+//   console.log(`stdout: ${data}`);
+// });
+
+// ls.stderr.on('data', (data) => {
+//   console.error(`stderr: ${data}`);
+// });
+
+// ls.on('close', (code) => {
+//   console.log(`child process exited with code ${code}`);
+// });
